4adbfe7008f7ade5bcefb0dcdf6811db9ffbee40
Amanda Zeigler <fromhtoz@gmail.com>
2023-02-13 08:13:06 -0500

    Add files via upload

--- test_investigations_post.py
+++ test_investigations_post.py
@@ -5,25 +5,29 @@ from investigations_api_wrapper import post_investigation, delete_investigation
 from investigation_response_validations import validate_investigation_json
 
 
-class TestPostInvestigations:
-    name_options = ["Ema Nympton", "José Smith"]  # ask for expected character set
-    amount_options = ["$14", "€20", "67.89"]  # ask for formatting options
-
-    @pytest.fixture()
-    def setup_investigation(self):
+class TestInvestigationsPost:
+    """
+    Test class for POST investigations endpoint
+    """
+    @pytest.fixture(params=[
+        ("Ema Nympton", "67.89"),
+        ("José Smith", "67.89"),
+        ("Jane Doe", "$67"),
+        ("Max Bacon", "€20.99")
+        ], ids=["ascii", "unicode", "currency no decimal", "euros"]
+    )
+    def setup_investigation(self, request):
         """
         investigation for use in happy path testings
         :return: <dict> {"expected_name": <str>, "expected_amount": <str>, "response": <request response>
         """
-        name = random.choice(self.name_options)
-        amount = random.choice(self.amount_options)
         post_response = post_investigation(
-            name=name,
-            amount=amount
+            name=request.param[0],
+            amount=request.param[1]
         )
         yield {
-            "expected_name": name,
-            "expected_amount": amount,
+            "expected_name": request.param[0],
+            "expected_amount": request.param[1],
             "response": post_response
         }
         delete_investigation(post_response.json()['investigationId'])
